c342d322e8d9e8cb21d44e2ba3faf8df553687b2
URAAM <112347754+Uraam@users.noreply.github.com>
2026-09-14 01:59:22 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -174,7 +174,7 @@ mkdir -p "$PREFIX/bin"
 
 TERMUX_DEB_URL=$(echo "$RELEASE_DATA" | jq -r '[.assets[] | select(.name | endswith(".deb")) | select(.name | contains("termux")) | .browser_download_url][0] // empty')
 
-if [[ -n "$TERMUX_DEB_URL" && "$TERMUX_DEB_URL" ! = "null" ]]; then
+if [[ -n "$TERMUX_DEB_URL" && "$TERMUX_DEB_URL" != "null" ]]; then
 printf "%b\n" "${GREEN}[+] Found Termux package:$(basename "$TERMUX_DEB_URL")${NC}"
 curl -L -o "$tmp_dir/uraam-termux.deb" "$TERMUX_DEB_URL"
 dpkg-i "$tmp_dir/uraam-termux.deb" || apt-get install -f -y
